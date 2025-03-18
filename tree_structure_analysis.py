58cbe9f77bee8b19261edb25b34c002044ecc41d
DanieleBarolo <barolo@csh.ac.at>
2025-03-18 10:19:30 +0100

    find_topic of a comment

--- tree_structure_analysis.py
+++ tree_structure_analysis.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 # Example User:
 
-user_name = "2bills"  # Ensure this matches the actual filename in selected_users_comments_compressed/
+user_name = "1Tiamo"  # Ensure this matches the actual filename in selected_users_comments_compressed/
 base_dir = "./selected_users_data/selected_users_comments_compressed"
 user_df = load_comments_from_jsonl_gz(user_name, base_dir)
 user_df
@@ -25,4 +25,31 @@ answer_comments_df._id.tolist()[0]
 for threat_comments in answer_comments_df._id.tolist()[:10]:
     print()
     trace_comment_thread(comment_id=threat_comments, collection = 'Breitbart', verbose=True, retrieve_article=True)
+
+
+
+### Access global topic of one comment
+
+# Find the topic
+from datetime import datetime
+import os 
+import pyarrow.feather as feather 
+import pyarrow.compute as pc
+
+
+answer_comments_df[['_id', 'raw_message']]
+sample_id = '1000981651'
+user_df[user_df['_id'] == '1000981651']['createdAt']
+full_date = user_df[user_df['_id'] == '1000981651']['createdAt'].iloc[0]
+mm_yy = full_date.strftime("%m%y")
+
+topic_path = "/Volumes/Untitled/seungwoong.ha/collmind/transform_global/breitbart/breitbart_new_s3_r19_h225_u20_t10"
+name_file = f"batch-{mm_yy}.arrow"
+
+gb = feather.read_table(os.path.join(topic_path, name_file))
+df = gb.to_pandas()
+df[df['id'] == sample_id]
+
+
+#####
 

85d042e1901137b983a59387cfabb3f8641afcbe
Victor Poulsen <victormoeller@gmail.com>
2025-03-27 16:28:14 +0100

    curating closed target results, working on network.

---
+++ group_topics/group_topics_closed.py
@@ -0,0 +1,47 @@
+import numpy as np 
+import pandas as pd 
+import json 
+import os 
+import re 
+
+# setup 
+path = '../Batch_calling/data/results/31499533/deepseek-r1-distill-llama-70b/closed_target/'
+file = 'batch_size_100_with_body_with_parent.jsonl'
+filepath = os.path.join(path, file)
+
+def process_jsonl(json_line): 
+    answer = json_line['response']['body']['choices'][0]['message']['content']
+    answer = re.split("</think>", answer)[1]
+    answer = re.search(r'```json\n(.*?)\n```', answer, re.DOTALL).group(1)
+    answer = json.loads(answer)
+    return answer 
+
+# load data
+data = []
+with open(filepath, 'r', encoding='utf-8') as f:
+    for line in f:
+        data.append(json.loads(line.strip()))
+        
+# take out results 
+from json import JSONDecodeError
+clean_data = []
+for num, ele in enumerate(data): 
+    try: 
+        clean_line = process_jsonl(ele) 
+        clean_data.append(clean_line)   
+    except JSONDecodeError: 
+        pass 
+
+# get all of the targets 
+topics = []
+for num, comment in enumerate(clean_data): 
+    comment = comment['results']
+    for topic in comment: 
+        print(topic)
+        target = topic['target']
+        stance = topic['stance']
+        topics.append([num, target, stance])
+        
+data = pd.DataFrame(topics, columns=['post_idx', 'target', 'stance'])
+data = data[data['stance'].isin(['FAVOR', 'AGAINST'])]
+data.to_csv('targets_closed.csv', index=False)
